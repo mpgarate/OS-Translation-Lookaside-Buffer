e721d9ab0720f81c53956377214f6708e91a6cbd
vagrant <mpgarate@gmail.com>
2013-12-09 12:39:28 -0500

    added name comment

--- page.c
+++ page.c
@@ -1,3 +1,9 @@
+/***********************************/
+/****** Two-level page table *******/
+/** Operating Systems Project #3 ***/
+/*** Michael Garate, Dec 9 2013 ****/
+/***********************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "types.h"
